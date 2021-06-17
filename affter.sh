00aeaacb251c9331631f6d6de7f096824151fd50
shabane <m.mohamadshabane@gmail.com>
2021-06-17 17:04:15 +0430

    the affter script write.

---
+++ affter.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo apt update
+sudo apt upgrade
+sudo apt dist-upgrade
+sudo apt install fish
+sudo apt install vlc
+sudo apt install git
+sudo apt install pip3
+sudo apt install tmux
+sudo apt install qbittorrent
+sudo apt install pv
+sudo apt install locate
+pip3 install howdoi
+pip3 install bpytop3
